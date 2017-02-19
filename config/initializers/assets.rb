907a4d571aaaaf11e0ae25f2a3c062eadbcfc388
jjuyeoni <cwa302@naver.com>
2017-02-19 04:34:03 +0000

    test4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( bootstrap/css/bootstrap.css )
 Rails.application.config.assets.precompile += %w( font-awesome/css/font-awesome.min.css )
-Rails.application.config.assets.precompile += %w( agency.css )
+Rails.application.config.assets.precompile += %w( agency.scss )
 
 Rails.application.config.assets.precompile += %w( jquery/jquery.min.js )
 Rails.application.config.assets.precompile += %w( bootstrap/js/bootstrap.min.js )
